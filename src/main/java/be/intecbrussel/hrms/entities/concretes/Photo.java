43c65c2c2f0c37e9aac9ce8d007a96c4cf990c01
Mustafa Genc <mustafagenc20@gmail.com>
2021-12-18 23:50:01 +0100

    Entities added

---
+++ src/main/java/be/intecbrussel/hrms/entities/concretes/Photo.java
@@ -0,0 +1,39 @@
+package be.intecbrussel.hrms.entities.concretes;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "photos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler","unemployed"})
+public class Photo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "photo_id")
+	private int photoId;
+	
+	@NotBlank
+	@NotNull
+	@Column(name = "photo_url")
+	private String photoUrl;
+	
+	@JsonIgnore
+	@Column(name = "upload_date", columnDefinition = "Date default CURRENT_DATE")
+	private LocalDate uploadDate = LocalDate.now();
+	
+	@OneToOne
+	@JoinColumn(name = "unemployed_id", referencedColumnName = "user_id")
+	private Unemployed unemployed;
+}
