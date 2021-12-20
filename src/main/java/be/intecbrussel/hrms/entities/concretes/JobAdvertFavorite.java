fd740c6d09b82005d94c0c1bdbcec04bc0096a90
Mustafa Genc <mustafagenc20@gmail.com>
2021-12-20 23:24:19 +0100

    Entities added

---
+++ src/main/java/be/intecbrussel/hrms/entities/concretes/JobAdvertFavorite.java
@@ -0,0 +1,28 @@
+package be.intecbrussel.hrms.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="job_advert_favorites")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JobAdvertFavorite {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="favorite_id")
+	private int favoriteId;
+	
+	@ManyToOne
+	@JoinColumn(name="unemployed_id", referencedColumnName = "user_id")
+	private Unemployed unemployed;
+	
+	@ManyToOne
+	@JoinColumn(name="advert_id")
+	private JobAdvertisement jobAdvertisement;
+}
