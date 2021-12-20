dcd78fdd15db3637f7c2c5a4da016cb887705284
Mustafa Genc <mustafagenc20@gmail.com>
2021-12-20 23:23:57 +0100

    Entities added

---
+++ src/main/java/be/intecbrussel/hrms/entities/concretes/User.java
@@ -0,0 +1,49 @@
+package be.intecbrussel.hrms.entities.concretes;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="users")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private int userId;
+	
+	@NotBlank
+	@NotNull
+	@Length(min=10, max = 10)
+	@Column(name = "phone_number")
+	private String phoneNumber;
+	
+	@NotBlank
+	@NotNull
+	@Email
+	@Column(name = "email")
+	private String email;
+	
+	@NotBlank
+	@NotNull
+	@Length(min=6, max=20)
+	@Column(name = "password")
+	@JsonIgnore
+	private String password;
+	
+	@JsonIgnore
+	@Column(name="mail_is_verify")
+	private Boolean mailIsVerify = false;
+}
---
+++ src/main/java/be/intecbrussel/hrms/entities/concretes/Verification.java
@@ -0,0 +1,33 @@
+package be.intecbrussel.hrms.entities.concretes;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "verifications")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Verification {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "verification_id")
+	private int verificationId;
+	
+	@JsonIgnore
+	@Column(name = "is_verified")
+	private boolean isVerified = false;
+	
+	@Column(name = "verification_code")
+	private String verificationCode;
+	
+	@Column(name = "user_id")
+	private int userId;
+
+}
