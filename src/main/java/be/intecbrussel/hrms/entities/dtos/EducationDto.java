eb3fd344a4f09f5e227f695388391e0cdfa4bc72
Mustafa Genc <mustafagenc20@gmail.com>
2021-12-28 17:06:15 +0100

    initial commits

---
+++ src/main/java/be/intecbrussel/hrms/entities/dtos/EducationDto.java
@@ -0,0 +1,34 @@
+package be.intecbrussel.hrms.entities.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EducationDto {
+	
+	private int unemployedId;
+	
+	@NotNull
+	@NotBlank
+	private String schoolName;
+	
+	@NotNull
+	@NotBlank
+	private String department;
+	
+	@NotNull
+	@NotBlank
+	private LocalDate startDate;
+	
+	@Nullable
+	private LocalDate graduatedDate;
+
+}
