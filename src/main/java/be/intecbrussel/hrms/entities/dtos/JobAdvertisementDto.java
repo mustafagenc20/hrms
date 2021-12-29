39ea207903390ec20c2d07ddbd88e48ade6a6f1e
Mustafa Genc <mustafagenc20@gmail.com>
2021-12-29 22:02:57 +0100

    initial commits

---
+++ src/main/java/be/intecbrussel/hrms/entities/dtos/JobAdvertisementDto.java
@@ -0,0 +1,24 @@
+package be.intecbrussel.hrms.entities.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JobAdvertisementDto {
+
+	private int employerId;	
+	private int minSalary;
+	private int maxSalary;
+	private int quota;
+	
+	private int cityId;
+	private int typeId;
+	private int timeId;
+	private int positionId;
+	
+	private String jobDescription;
+	private String lastApplication;
+}
