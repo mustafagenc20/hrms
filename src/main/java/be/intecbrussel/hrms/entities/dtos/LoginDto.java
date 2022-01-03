10158a57ca9fb26e72db9bdc91c4c3cb9682b0a6
Mustafa Genc <mustafagenc20@gmail.com>
2022-01-03 23:26:27 +0100

    initial commits

---
+++ src/main/java/be/intecbrussel/hrms/entities/dtos/LoginDto.java
@@ -0,0 +1,14 @@
+package be.intecbrussel.hrms.entities.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDto {
+
+	private String email;
+	private String password;
+}
