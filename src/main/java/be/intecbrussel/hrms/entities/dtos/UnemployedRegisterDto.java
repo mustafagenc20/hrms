ea898e321327667ccb7cdde56b4fb3f9198bf79c
Mustafa Genc <mustafagenc20@gmail.com>
2022-01-03 23:26:38 +0100

    initial commits

---
+++ src/main/java/be/intecbrussel/hrms/entities/dtos/UnemployedRegisterDto.java
@@ -0,0 +1,50 @@
+package be.intecbrussel.hrms.entities.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UnemployedRegisterDto {
+	
+	@Email
+	@NotNull
+	@NotBlank
+	private String email;
+	
+	@NotNull
+	@NotBlank
+	@Length(min=6, max=20)
+	private String password;
+	
+	@NotNull
+	@NotBlank
+	@Length(min=2)
+	private String firstName;
+	
+	@NotNull
+	@NotBlank
+	@Length(min=2)
+	private String lastName;
+	
+	@NotNull
+	@NotBlank
+	@Length(min=11, max = 11)
+	private String nationalityId;
+	
+	@NotNull
+	private LocalDate birthDate;
+	
+	@NotNull
+	@NotBlank
+	@Length(min=10, max=10)
+	private String phoneNumber;
+}
