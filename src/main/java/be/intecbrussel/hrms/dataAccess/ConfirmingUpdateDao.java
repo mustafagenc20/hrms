3502503a05808e309a53f4317ba77d27da0f36c4
Mustafa Genc <mustafagenc20@gmail.com>
2022-01-16 23:03:09 +0100

    initial commits

---
+++ src/main/java/be/intecbrussel/hrms/dataAccess/ConfirmingUpdateDao.java
@@ -0,0 +1,6 @@
+package be.intecbrussel.hrms.dataAccess;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConfirmingUpdateDao extends JpaRepository<ConfirmingUpdate, Integer> {
+}
