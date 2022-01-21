ce9459409c3ae87a747a128f3cf7ec40c772d74c
Mustafa Genc <mustafagenc20@gmail.com>
2022-01-21 23:19:49 +0100

    initial commits

--- src/main/java/be/intecbrussel/hrms/dataAccess/EmployerDao.java
+++ src/main/java/be/intecbrussel/hrms/dataAccess/EmployerDao.java
@@ -1,5 +1,6 @@
 package be.intecbrussel.hrms.dataAccess;
 
+import be.intecbrussel.hrms.entities.concretes.Employer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
