a28385c0ecddffc5ac8480dddcda4801cb980a7f
Mustafa Genc <mustafagenc20@gmail.com>
2022-01-21 23:19:31 +0100

    initial commits

--- src/main/java/be/intecbrussel/hrms/dataAccess/ConfirmingJobAdvertisementDao.java
+++ src/main/java/be/intecbrussel/hrms/dataAccess/ConfirmingJobAdvertisementDao.java
@@ -1,5 +1,6 @@
 package be.intecbrussel.hrms.dataAccess;
 
+import be.intecbrussel.hrms.entities.concretes.ConfirmingJobAdvertisement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConfirmingJobAdvertisementDao extends JpaRepository<ConfirmingJobAdvertisement, Integer>{
