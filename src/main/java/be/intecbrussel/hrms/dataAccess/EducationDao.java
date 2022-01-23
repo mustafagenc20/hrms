d8222a38c2327fcc8533c9aeee3f6c127868949c
Mustafa Genc <mustafagenc20@gmail.com>
2022-01-23 22:55:03 +0100

    initial commits

--- src/main/java/be/intecbrussel/hrms/dataAccess/EducationDao.java
+++ src/main/java/be/intecbrussel/hrms/dataAccess/EducationDao.java
@@ -1,11 +1,12 @@
 package be.intecbrussel.hrms.dataAccess;
 
+import be.intecbrussel.hrms.entities.concretes.Education;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface EducationDao extends JpaRepository<Education, Integer> {
 
-    Education getBySchoolNameAndDepartmentAndUnemployed_UserId(String schoolName, String departmen, int userId);
+    Education getBySchoolNameAndDepartmentAndUnemployed_UserId(String schoolName, String department, int userId);
     List<Education> getByUnemployed_UserIdOrderByGraduatedDateDesc(int unemployedId);
 }
