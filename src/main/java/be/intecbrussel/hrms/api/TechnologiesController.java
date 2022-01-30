e825c6ddef21d9bd9b38f4b2b8aa15dff314a7b3
Mustafa Genc <mustafagenc20@gmail.com>
2022-01-30 22:22:00 +0100

    initial commits

---
+++ src/main/java/be/intecbrussel/hrms/api/TechnologiesController.java
@@ -0,0 +1,47 @@
+package be.intecbrussel.hrms.api;
+
+import be.intecbrussel.hrms.business.abstracts.TechnologyService;
+import be.intecbrussel.hrms.core.utilities.util.ResponseEntityReturn;
+import be.intecbrussel.hrms.entities.dtos.TechnologyDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/technologies/")
+@CrossOrigin
+public class TechnologiesController {
+
+    private TechnologyService technologyService;
+
+    @Autowired
+    public TechnologiesController(TechnologyService technologyService) {
+        super();
+        this.technologyService = technologyService;
+    }
+
+    @PostMapping("addTechnology")
+    public ResponseEntity<?> addTechnology(@RequestBody TechnologyDto technologyDto) {
+        return ResponseEntityReturn.checkResult(this.technologyService.addTechnology(technologyDto));
+    }
+
+    @PutMapping("updateTechnology")
+    public ResponseEntity<?> updateTechnology(@RequestBody TechnologyDto technologyDto, @RequestParam int technologyId) {
+        return ResponseEntityReturn.checkResult(this.technologyService.updateTechnology(technologyDto, technologyId));
+    }
+
+    @DeleteMapping("deleteTechnology")
+    public ResponseEntity<?> deleteTechnology(@RequestParam int technologyId) {
+        return ResponseEntityReturn.checkResult(this.technologyService.deleteTechnology(technologyId));
+    }
+
+    @GetMapping("getAllTechnologies")
+    public ResponseEntity<?> getAll() {
+        return ResponseEntity.ok(this.technologyService.getAll());
+    }
+
+    @GetMapping("getByUnemployedId")
+    public ResponseEntity<?> getByUnemployedId(@RequestParam int unemployedId) {
+        return ResponseEntity.ok(this.technologyService.getByUnemployedId(unemployedId));
+    }
+}
