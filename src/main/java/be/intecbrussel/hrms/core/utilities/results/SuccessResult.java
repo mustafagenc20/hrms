411b17d96ed057478846b12d191191eaa835de01
Mustafa Genc <mustafagenc20@gmail.com>
2022-02-01 22:38:57 +0100

    initial commits

---
+++ src/main/java/be/intecbrussel/hrms/core/utilities/results/SuccessResult.java
@@ -0,0 +1,12 @@
+package be.intecbrussel.hrms.core.utilities.results;
+
+public class SuccessResult extends Result {
+
+    public SuccessResult() {
+        super(true);
+    }
+
+    public SuccessResult(String message) {
+        super(true, message);
+    }
+}
