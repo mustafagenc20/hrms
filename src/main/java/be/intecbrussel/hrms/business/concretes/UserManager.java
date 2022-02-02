fa2378a7a793a162165320879265d0095ea12ff8
Mustafa Genc <mustafagenc20@gmail.com>
2022-02-02 23:08:50 +0100

    initial commits

--- src/main/java/be/intecbrussel/hrms/business/concretes/UserManager.java
+++ src/main/java/be/intecbrussel/hrms/business/concretes/UserManager.java
@@ -1,28 +1,29 @@
 package be.intecbrussel.hrms.business.concretes;
 
+import be.intecbrussel.hrms.business.abstracts.UserService;
+import be.intecbrussel.hrms.core.utilities.results.DataResult;
+import be.intecbrussel.hrms.core.utilities.results.SuccessDataResult;
+import be.intecbrussel.hrms.dataAccess.UserDao;
+import be.intecbrussel.hrms.entities.concretes.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class UserManager implements UserService{
+@RequiredArgsConstructor
+public class UserManager implements UserService {
 
-    private UserDao userDao;
-
-    @Autowired
-    public UserManager(UserDao userDao) {
-        super();
-        this.userDao = userDao;
-    }
+    private final UserDao userDao;
 
     @Override
     public DataResult<List<User>> getAll() {
-        return new SuccessDataResult<List<User>>(this.userDao.findAll(), "Bütün kullanıcılar listelendi.");
+        return new SuccessDataResult<List<User>>(this.userDao.findAll(), "All users are listed.");
     }
 
     @Override
     public DataResult<List<User>> getByMailConfirmed() {
-        return new SuccessDataResult<List<User>>(this.userDao.findByMailIsVerifyTrue(), "Onaylanmış kullanıcılar listelendi.");
+        return new SuccessDataResult<List<User>>(this.userDao.findByMailIsVerifyTrue(), "Approved users are listed.");
     }
 }
