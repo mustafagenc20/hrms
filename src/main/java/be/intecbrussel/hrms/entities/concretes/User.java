825efae3246d3f602379090003271583e2138e3e
Mustafa Genc <mustafagenc20@gmail.com>
2022-02-02 23:08:59 +0100

    initial commits

--- src/main/java/be/intecbrussel/hrms/entities/concretes/User.java
+++ src/main/java/be/intecbrussel/hrms/entities/concretes/User.java
@@ -22,7 +22,7 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "user_id")
-	private int userId;
+	private Integer userId;
 	
 	@NotBlank
 	@NotNull
