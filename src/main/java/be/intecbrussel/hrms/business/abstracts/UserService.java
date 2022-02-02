16fef1e0c2790fcc20f4eaca44539cec90610b79
Mustafa Genc <mustafagenc20@gmail.com>
2022-02-02 23:09:04 +0100

    initial commits

--- src/main/java/be/intecbrussel/hrms/business/abstracts/UserService.java
+++ src/main/java/be/intecbrussel/hrms/business/abstracts/UserService.java
@@ -1,5 +1,8 @@
 package be.intecbrussel.hrms.business.abstracts;
 
+import be.intecbrussel.hrms.core.utilities.results.DataResult;
+import be.intecbrussel.hrms.entities.concretes.User;
+
 import java.util.List;
 
 public interface UserService {
