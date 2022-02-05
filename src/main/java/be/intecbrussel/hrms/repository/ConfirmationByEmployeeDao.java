4aa56da9aab27d1d73f3f646df37a41b2a109385
Mustafa Genc <mustafagenc20@gmail.com>
2022-02-05 22:51:49 +0100

    initial commits

--- src/main/java/be/intecbrussel/hrms/repository/ConfirmationByEmployeeDao.java
+++ src/main/java/be/intecbrussel/hrms/repository/ConfirmationByEmployeeDao.java
@@ -1,5 +1,6 @@
-package be.intecbrussel.hrms.dataAccess;
+package be.intecbrussel.hrms.repository;
 
+import be.intecbrussel.hrms.model.entities.ConfirmationByEmployee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConfirmationByEmployeeDao extends JpaRepository<ConfirmationByEmployee, Integer> {
