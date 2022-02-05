e78b833bebec13243586cd4a4b820dc4344b7667
Mustafa Genc <mustafagenc20@gmail.com>
2022-02-05 22:51:38 +0100

    initial commits

--- src/main/java/be/intecbrussel/hrms/repository/CoverLetterDao.java
+++ src/main/java/be/intecbrussel/hrms/repository/CoverLetterDao.java
@@ -1,6 +1,6 @@
-package be.intecbrussel.hrms.dataAccess;
+package be.intecbrussel.hrms.repository;
 
-import be.intecbrussel.hrms.entities.concretes.CoverLetter;
+import be.intecbrussel.hrms.model.entities.CoverLetter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CoverLetterDao extends JpaRepository<CoverLetter, Integer> {
