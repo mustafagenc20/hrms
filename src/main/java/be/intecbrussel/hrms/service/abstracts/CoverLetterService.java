fe71e63517c14afdeaf6d6b30591e269a8ed0379
Mustafa Genc <mustafagenc20@gmail.com>
2022-02-10 17:54:38 +0100

    Initial commits

--- src/main/java/be/intecbrussel/hrms/service/abstracts/CoverLetterService.java
+++ src/main/java/be/intecbrussel/hrms/service/abstracts/CoverLetterService.java
@@ -1,9 +1,9 @@
-package be.intecbrussel.hrms.business.abstracts;
+package be.intecbrussel.hrms.service.abstracts;
 
 import be.intecbrussel.hrms.core.utilities.results.DataResult;
 import be.intecbrussel.hrms.core.utilities.results.Result;
-import be.intecbrussel.hrms.entities.concretes.CoverLetter;
-import be.intecbrussel.hrms.entities.dtos.CoverLetterDto;
+import be.intecbrussel.hrms.model.entities.CoverLetter;
+import be.intecbrussel.hrms.model.dtos.CoverLetterDto;
 
 import java.util.List;
 
