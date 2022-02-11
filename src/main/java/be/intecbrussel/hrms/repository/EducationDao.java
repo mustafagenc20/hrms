623123305b683743604d14d9f5853f62dc0a530c
Mustafa Genc <mustafagenc20@gmail.com>
2022-02-12 00:29:05 +0100

    Initial commits

--- src/main/java/be/intecbrussel/hrms/repository/EducationDao.java
+++ src/main/java/be/intecbrussel/hrms/repository/EducationDao.java
@@ -1,6 +1,6 @@
-package be.intecbrussel.hrms.dataAccess;
+package be.intecbrussel.hrms.repository;
 
-import be.intecbrussel.hrms.entities.concretes.Education;
+import be.intecbrussel.hrms.model.entities.Education;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
