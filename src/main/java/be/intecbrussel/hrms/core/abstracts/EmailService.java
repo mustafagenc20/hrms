c9193c2f6ffa1581fd6ffb57c29fc0aae33cc87f
Mustafa Genc <mustafagenc20@gmail.com>
2022-02-13 15:44:39 +0100

    Initial commits

--- src/main/java/be/intecbrussel/hrms/core/abstracts/EmailService.java
+++ src/main/java/be/intecbrussel/hrms/core/abstracts/EmailService.java
@@ -1,5 +1,7 @@
 package be.intecbrussel.hrms.core.abstracts;
 
+import be.intecbrussel.hrms.model.entities.User;
+
 public interface EmailService {
 
     void sendVerifyEmail(User user, String code);
