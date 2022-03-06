9bce69970b88d900e37e51314144deb6e834b35f
Mustafa Genc <mustafagenc20@gmail.com>
2022-03-06 22:44:31 +0100

    Initial commits

--- src/main/java/be/intecbrussel/hrms/service/concretes/JobAdvertFavoriteManager.java
+++ src/main/java/be/intecbrussel/hrms/service/concretes/JobAdvertFavoriteManager.java
@@ -23,34 +23,34 @@ public class JobAdvertFavoriteManager implements JobAdvertFavoriteService {
     public Result addFavorite(int unemployedId, int advertId) {
 
         if(!this.unemployedDao.existsById(unemployedId)) {
-            return new ErrorResult("User not found.");
+            return new ErrorResult("User not found");
         }else if (!this.advertisementDao.existsById(advertId)) {
-            return new ErrorResult("Advertisement not found.");
+            return new ErrorResult("Advertisement not found");
         }else if(this.favoriteDao.existsByUnemployed_UserIdAndJobAdvertisement_AdvertId(unemployedId, advertId)) {
-            return new ErrorResult("The advertisement has already been added to favourites.");
+            return new ErrorResult("The advertisement has already been added to favourites");
         }
 
         JobAdvertFavorite advertFavorite = new JobAdvertFavorite();
         advertFavorite.setUnemployed(this.unemployedDao.getOne(unemployedId));
         advertFavorite.setJobAdvertisement(this.advertisementDao.getOne(advertId));
         this.favoriteDao.save(advertFavorite);
-        return new SuccessResult("The advertisement has been added to favourites.");
+        return new SuccessResult("The advertisement has been added to favourites");
     }
 
     @Override
     public Result deleteFavorite(int favoriteId) {
         if(!this.favoriteDao.existsById(favoriteId)) {
-            return new ErrorResult("The advertisement you want to delete is not found in favourites.");
+            return new ErrorResult("The advertisement you want to delete is not found in favourites");
         }
         this.favoriteDao.deleteById(favoriteId);
-        return new SuccessResult("Advertisement has been successfully removed from favourites.");
+        return new SuccessResult("Advertisement has been successfully removed from favourites");
     }
 
     @Override
     public DataResult<List<JobAdvertFavorite>> getByUnemployedId(int unemployedId) {
         if(!this.unemployedDao.existsById(unemployedId)) {
-            return new ErrorDataResult<List<JobAdvertFavorite>>("The user you entered was not found.");
+            return new ErrorDataResult<List<JobAdvertFavorite>>("The user you entered was not found");
         }
-        return new SuccessDataResult<List<JobAdvertFavorite>>(this.favoriteDao.getByUnemployed_UserId(unemployedId), "User's favorite postings are listed.");
+        return new SuccessDataResult<List<JobAdvertFavorite>>(this.favoriteDao.getByUnemployed_UserId(unemployedId), "User's favorite postings are listed");
     }
 }
