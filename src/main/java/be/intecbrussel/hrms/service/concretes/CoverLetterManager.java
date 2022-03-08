4a63dac2297ea791f0e9957c28d6eba39061d8b8
Mustafa Genc <mustafagenc20@gmail.com>
2022-03-08 23:38:06 +0100

    Initial commits

--- src/main/java/be/intecbrussel/hrms/service/concretes/CoverLetterManager.java
+++ src/main/java/be/intecbrussel/hrms/service/concretes/CoverLetterManager.java
@@ -21,38 +21,38 @@ public class CoverLetterManager implements CoverLetterService {
     @Override
     public Result addCoverLetter(CoverLetterDto coverLetterDto) {
         if (this.coverLetterDao.getByUnemployed_UserId(coverLetterDto.getUnemployedId()) != null) {
-            return new ErrorResult("You have already added a cover letter.");
+            return new ErrorResult("You have already added a cover letter");
         }
         CoverLetter coverLetter = new CoverLetter();
         coverLetter.setLetterContent(coverLetterDto.getLetterContent());
         coverLetter.setUnemployed(this.unemployedDao.getOne(coverLetterDto.getUnemployedId()));
 
         this.coverLetterDao.save(coverLetter);
-        return new SuccessResult("Added cover letter.");
+        return new SuccessResult("Added cover letter");
     }
 
     @Override
     public DataResult<List<CoverLetter>> getAll() {
-        return new SuccessDataResult<List<CoverLetter>>(this.coverLetterDao.findAll(), "The cover letters are listed.");
+        return new SuccessDataResult<List<CoverLetter>>(this.coverLetterDao.findAll(), "The cover letters are listed");
     }
 
     @Override
     public DataResult<CoverLetter> getByUnemployedId(int unemployedId) {
         return new SuccessDataResult<CoverLetter>(this.coverLetterDao.getByUnemployed_UserId(unemployedId),
-                "The cover letter of the job seeker is listed.");
+                "The cover letter of the job seeker is listed");
     }
 
     @Override
     public Result updateCoverLetter(CoverLetterDto coverLetterDto) {
         if (this.coverLetterDao.getByUnemployed_UserId(coverLetterDto.getUnemployedId()) == null) {
-            return new ErrorResult("You must add a cover letter first.");
+            return new ErrorResult("You must add a cover letter first");
         }
         CoverLetter coverLetter = this.coverLetterDao.getByUnemployed_UserId(coverLetterDto.getUnemployedId());
         if (coverLetter.getLetterContent().equals(coverLetterDto.getLetterContent())) {
-            return new ErrorResult("You forgot to update.");
+            return new ErrorResult("You forgot to update");
         }
         coverLetter.setLetterContent(coverLetterDto.getLetterContent());
         this.coverLetterDao.save(coverLetter);
-        return new SuccessResult("The cover letter has been updated.");
+        return new SuccessResult("The cover letter has been updated");
     }
 }
